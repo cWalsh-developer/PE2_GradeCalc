aff537c5ed2dbd9068064f82df4c99e0465233df
Connor Walsh <connor_w_98@hotmail.com>
2024-03-12 05:47:56 +0000

    redesigned the registration page to better fit the wireframes, added JavaScript form validation and added a favicon icon in the tab bar of each page.

--- controller/JavaScript/PE2_Calc_JavaScript.js
+++ controller/JavaScript/PE2_Calc_JavaScript.js
@@ -1,64 +1,78 @@
-function buttonClick(evt) {
-  evt.preventDefault(); // This line will prevent any action such as refreshing the page after submitting the form.
-  var password = document.getElementById("password").value;
-  var confirmPassword = document.getElementById("passwordConfirm").value;
-  password = password.trim();
-  confirmPassword = confirmPassword.trim();
+/* Creating a function to validate the registration form inputs*/
 
-  var nameText = document.getElementById("name").value;
-  var emailText = document.getElementById("email").value;
-  nameText = nameText.trim();
-  emailText = emailText.trim();
+function validateRegistrationForm() {
+  var password = document.forms["regForm"]["password"].value;
+  var confirmPassword = document.forms["regForm"]["passwordConfirm"].value;
+  var nameText = document.forms["regForm"]["fname"].value;
+  var emailText = document.forms["regForm"]["email"].value;
+  var lastNameText = document.forms["regForm"]["lname"].value;
 
-  if(nameText == "Enter your name" || nameText == "")
+  if (nameText == null || nameText.trim() == "") 
   {
-    alert("Please enter your name")
-  }
-  else if (emailText == "Enter your email" || emailText == "")
+    document.getElementById("fnameInput").style.display = "block";
+    document.getElementById("break0").style.display = "none";
+  } 
+  if (lastNameText == null || lastNameText.trim() == "") 
   {
-    alert("Please enter an email address");
-  }
-  else if(emailText.includes("@") && emailText.includes("."))
+    document.getElementById("lnameInput").style.display = "block";
+    document.getElementById("break1").style.display = "none";
+  } 
+  if (emailText == null || emailText.trim() == "") 
   {
-    if(password == "")
-    {
-      alert("Please enter a password")
-    }
-    else if(confirmPassword == "")
-    {
-      alert("Please confirm password")
-    }
-    else if (confirmPassword != password) {
-      alert("Your passwords do not match");
-    }
-    else
-    {
-      window.location.href = "#";
-    }
-  }
-  else
+    document.getElementById("emailInput").style.display = "block";
+    document.getElementById("break2").style.display = "none";
+  } 
+  else if (!emailText.includes("@") && !emailText.includes(".")) 
   {
-    alert("Please enter a valid email address")
+    document.getElementById("emailInputCheck").style.display = "block";
+    document.getElementById("break2").style.display = "none";
+  } 
+  if (password == null || password.trim() == "") 
+  {
+    document.getElementById("passwordInput").style.display = "block";
+    document.getElementById("break3").style.display = "none";
+  } 
+  if (confirmPassword == null || confirmPassword.trim() == "") 
+  {
+    document.getElementById("confirmPasswordInput").style.display = "block";
+    document.getElementById("break4").style.display = "none";
+  } 
+  else if (password.trim() != confirmPassword.trim()) 
+  {
+    document.getElementById("confirmPasswordCheck").style.display = "block";
+    document.getElementById("break4").style.display = "none";
   }
+  
 }
 
-function nameFieldClick()
+//Creating an additional function to make the error messages disappear when the user starts to retype their information
+function vanishErrorsOnClick()
 {
-  if(document.getElementById("name").value == "Enter your name")
-  {
-    document.getElementById("name").value = "";
-  }
+    document.getElementById("fnameInput").style.display = "none";
+    document.getElementById("break0").style.display = "block";
+
+    document.getElementById("lnameInput").style.display = "none";
+    document.getElementById("break1").style.display = "block";
+
+    document.getElementById("emailInput").style.display = "none";
+    document.getElementById("break2").style.display = "block";
+
+    document.getElementById("passwordInput").style.display = "none";
+    document.getElementById("break3").style.display = "block";
+
+    document.getElementById("confirmPasswordInput").style.display = "none";
+    document.getElementById("break4").style.display = "block";
+
+    document.getElementById("confirmPasswordCheck").style.display = "none";
+    document.getElementById("break4").style.display = "block";
  
-}
-function emailFieldClick()
-{
-  if(document.getElementById("email").value == "Enter your email")
-  {
-    document.getElementById("email").value = "";
-  }
+    document.getElementById("emailInputCheck").style.display = "none";
+    document.getElementById("break2").style.display = "block";
 }
 
-document.getElementById("name").onclick = nameFieldClick;
-document.getElementById("email").onclick = emailFieldClick;
-document.getElementById("submit").addEventListener("click", buttonClick);
-
+document.getElementById("submit").onclick = validateRegistrationForm;
+document.getElementById("fname").onclick = vanishErrorsOnClick;
+document.getElementById("lname").onclick = vanishErrorsOnClick;
+document.getElementById("email").onclick = vanishErrorsOnClick;
+document.getElementById("password").onclick = vanishErrorsOnClick;
+document.getElementById("passwordConfirm").onclick = vanishErrorsOnClick;
