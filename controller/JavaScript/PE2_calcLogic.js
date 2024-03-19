77d0ca75dcceea9b672a2cb6c95008784f2ccfa3
Denis Nicolae <denisnic801@gmail.com>
2024-03-19 22:26:14 +0000

    calculation logic so an average of % is now calculated

--- controller/JavaScript/PE2_calcLogic.js
+++ controller/JavaScript/PE2_calcLogic.js
@@ -37,10 +37,10 @@ function calculateGrades()
   var quizMarkL5M4 = document.getElementById("quizInputL5M4").value;
   var courseworkMarkL5M4 = document.getElementById("courseworkInputL5M4").value;
 
-  var markL5M1 = parseInt(testMarkL5M1) + parseInt(quizMarkL5M1) + parseInt(courseworkMarkL5M1);
-  var markL5M2 = parseInt(testMarkL5M2) + parseInt(quizMarkL5M2) + parseInt(courseworkMarkL5M2);
-  var markL5M3 = parseInt(testMarkL5M3) + parseInt(quizMarkL5M3) + parseInt(courseworkMarkL5M3);
-  var markL5M4 = parseInt(testMarkL5M4) + parseInt(quizMarkL5M4) + parseInt(courseworkMarkL5M4);
+  var markL5M1 = (parseInt(testMarkL5M1) + parseInt(quizMarkL5M1) + parseInt(courseworkMarkL5M1)) / 3;
+  var markL5M2 = (parseInt(testMarkL5M2) + parseInt(quizMarkL5M2) + parseInt(courseworkMarkL5M2)) / 3;
+  var markL5M3 = (parseInt(testMarkL5M3) + parseInt(quizMarkL5M3) + parseInt(courseworkMarkL5M3)) / 3;
+  var markL5M4 = (parseInt(testMarkL5M4) + parseInt(quizMarkL5M4) + parseInt(courseworkMarkL5M4)) / 3;
 
   if(markL5M1 < markL5M2 && markL5M1 < markL5M3 && markL5M1 < markL5M4) //if the first module has the lowest mark
   {
@@ -80,10 +80,10 @@ function calculateGrades()
   var quizMarkL6M4 = document.getElementById("quizInputL6M4").value;
   var courseworkMarkL6M4 = document.getElementById("courseworkInputL6M4").value;
 
-  var markL6M1 = parseInt(testMarkL6M1) + parseInt(quizMarkL6M1) + parseInt(courseworkMarkL6M1);
-  var markL6M2 = parseInt(testMarkL6M2) + parseInt(quizMarkL6M2) + parseInt(courseworkMarkL6M2);
-  var markL6M3 = parseInt(testMarkL6M3) + parseInt(quizMarkL6M3) + parseInt(courseworkMarkL6M3);
-  var markL6M4 = parseInt(testMarkL6M4) + parseInt(quizMarkL6M4) + parseInt(courseworkMarkL6M4);
+  var markL6M1 = (parseInt(testMarkL6M1) + parseInt(quizMarkL6M1) + parseInt(courseworkMarkL6M1)) / 3;
+  var markL6M2 = (parseInt(testMarkL6M2) + parseInt(quizMarkL6M2) + parseInt(courseworkMarkL6M2)) / 3;
+  var markL6M3 = (parseInt(testMarkL6M3) + parseInt(quizMarkL6M3) + parseInt(courseworkMarkL6M3)) / 3;
+  var markL6M4 = (parseInt(testMarkL6M4) + parseInt(quizMarkL6M4) + parseInt(courseworkMarkL6M4)) / 3;
 
   if(markL6M1 < markL6M2 && markL6M1 < markL6M3 && markL6M1 < markL6M4) //if the first module has the lowest mark
   {
