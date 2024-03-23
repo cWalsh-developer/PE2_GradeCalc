7a57200c11cfe7c1f26d9e51ac2399359e355b43
JustABoredPerson <nikitosm1604@gmail.com>
2024-03-23 15:09:08 +0000

    Added Scrollbar to teacher view and removed logic to allow for grade prediction without entering all values

--- controller/JavaScript/PE2_calcLogic.js
+++ controller/JavaScript/PE2_calcLogic.js
@@ -21,21 +21,23 @@ function calculateGrades()
 
   //here starts the calculation for level 5/////////////////////////////////////////////////////////////////////////
 
-  var testMarkL5M1 = document.getElementById("testInputL5M1").value;
-  var quizMarkL5M1 = document.getElementById("quizInputL5M1").value;
-  var courseworkMarkL5M1 = document.getElementById("courseworkInputL5M1").value;
+  var testMarkL5M1 = parseFloat(document.getElementById("testInputL5M1").value) || 0;
+  var quizMarkL5M1 = parseFloat(document.getElementById("quizInputL5M1").value) || 0;
+  var courseworkMarkL5M1 = parseFloat(document.getElementById("courseworkInputL5M1").value) || 0;
 
-  var testMarkL5M2 = document.getElementById("testInputL5M2").value;
-  var quizMarkL5M2 = document.getElementById("quizInputL5M2").value;
-  var courseworkMarkL5M2 = document.getElementById("courseworkInputL5M2").value;
+  var testMarkL5M2 = parseFloat(document.getElementById("testInputL5M2").value) || 0;
+  var quizMarkL5M2 = parseFloat(document.getElementById("quizInputL5M2").value) || 0;
+  var courseworkMarkL5M2 = parseFloat(document.getElementById("courseworkInputL5M2").value) || 0;
 
-  var testMarkL5M3 = document.getElementById("testInputL5M3").value;
-  var quizMarkL5M3 = document.getElementById("quizInputL5M3").value;
-  var courseworkMarkL5M3 = document.getElementById("courseworkInputL5M3").value;
+  var testMarkL5M3 = parseFloat(document.getElementById("testInputL5M3").value) || 0;
+  var quizMarkL5M3 = parseFloat(document.getElementById("quizInputL5M3").value) || 0;
+  var courseworkMarkL5M3 = parseFloat(document.getElementById("courseworkInputL5M3").value) || 0;
 
-  var testMarkL5M4 = document.getElementById("testInputL5M4").value;
-  var quizMarkL5M4 = document.getElementById("quizInputL5M4").value;
-  var courseworkMarkL5M4 = document.getElementById("courseworkInputL5M4").value;
+
+  var testMarkL5M4 = parseFloat(document.getElementById("testInputL5M4").value) || 0;
+  var quizMarkL5M4 = parseFloat(document.getElementById("quizInputL5M4").value) || 0;
+  var courseworkMarkL5M4 = parseFloat(document.getElementById("courseworkInputL5M4").value) || 0;
+
 
   var markL5M1 = (parseInt(testMarkL5M1) + parseInt(quizMarkL5M1) + parseInt(courseworkMarkL5M1)) / 3;
   var markL5M2 = (parseInt(testMarkL5M2) + parseInt(quizMarkL5M2) + parseInt(courseworkMarkL5M2)) / 3;
@@ -64,21 +66,32 @@ function calculateGrades()
 
   //here starts the calculation for level 6/////////////////////////////////////////////////////////////////////////
 
-  var testMarkL6M1 = document.getElementById("testInputL6M1").value;
-  var quizMarkL6M1 = document.getElementById("quizInputL6M1").value;
-  var courseworkMarkL6M1 = document.getElementById("courseworkInputL6M1").value;
+  // For testMarkL6M1, quizMarkL6M1, courseworkMarkL6M1
+  var testMarkL6M1 = parseFloat(document.getElementById("testInputL6M1").value) || 0;
+  var quizMarkL6M1 = parseFloat(document.getElementById("quizInputL6M1").value) || 0;
+  var courseworkMarkL6M1 = parseFloat(document.getElementById("courseworkInputL6M1").value) || 0;
 
-  var testMarkL6M2 = document.getElementById("testInputL6M2").value;
-  var quizMarkL6M2 = document.getElementById("quizInputL6M2").value;
-  var courseworkMarkL6M2 = document.getElementById("courseworkInputL6M2").value;
+  // Repeat the same for testMarkL6M2, quizMarkL6M2, courseworkMarkL6M2, etc.
 
-  var testMarkL6M3 = document.getElementById("testInputL6M3").value;
-  var quizMarkL6M3 = document.getElementById("quizInputL6M3").value;
-  var courseworkMarkL6M3 = document.getElementById("courseworkInputL6M3").value;
+  // For testMarkL6M2, quizMarkL6M2, courseworkMarkL6M2
+  var testMarkL6M2 = parseFloat(document.getElementById("testInputL6M2").value) || 0;
+  var quizMarkL6M2 = parseFloat(document.getElementById("quizInputL6M2").value) || 0;
+  var courseworkMarkL6M2 = parseFloat(document.getElementById("courseworkInputL6M2").value) || 0;
 
-  var testMarkL6M4 = document.getElementById("testInputL6M4").value;
-  var quizMarkL6M4 = document.getElementById("quizInputL6M4").value;
-  var courseworkMarkL6M4 = document.getElementById("courseworkInputL6M4").value;
+  // Repeat the same for testMarkL6M3, quizMarkL6M3, courseworkMarkL6M3
+
+  // For testMarkL6M3, quizMarkL6M3, courseworkMarkL6M3
+  var testMarkL6M3 = parseFloat(document.getElementById("testInputL6M3").value) || 0;
+  var quizMarkL6M3 = parseFloat(document.getElementById("quizInputL6M3").value) || 0;
+  var courseworkMarkL6M3 = parseFloat(document.getElementById("courseworkInputL6M3").value) || 0;
+
+  // Repeat the same for testMarkL6M4, quizMarkL6M4, courseworkMarkL6M4
+
+  // For testMarkL6M4, quizMarkL6M4, courseworkMarkL6M4
+  var testMarkL6M4 = parseFloat(document.getElementById("testInputL6M4").value) || 0;
+  var quizMarkL6M4 = parseFloat(document.getElementById("quizInputL6M4").value) || 0;
+  var courseworkMarkL6M4 = parseFloat(document.getElementById("courseworkInputL6M4").value) || 0;
+
 
   var markL6M1 = (parseInt(testMarkL6M1) + parseInt(quizMarkL6M1) + parseInt(courseworkMarkL6M1)) / 3;
   var markL6M2 = (parseInt(testMarkL6M2) + parseInt(quizMarkL6M2) + parseInt(courseworkMarkL6M2)) / 3;
@@ -109,6 +122,7 @@ function calculateGrades()
 
   //result5 and result6 are 2 paragraphs I added for testing purposes, feel free to modify them to suit your design :)
 
+
   document.getElementById("result5").innerHTML = "Level 5 out of 20% = " + finalL5.toFixed(2) +"%";
   document.getElementById("result6").innerHTML = "Level 6 out of 80% = " + finalL6.toFixed(2) +"%";
   document.getElementById("displayResult").innerHTML = "You got " + finalAverage +"%";
@@ -138,112 +152,109 @@ function calculatePrediction()
     var pass = 40;
 
     //error checking to make sure grades are inputted before prediction can be calculated
-    if (isNaN(finalL5) || isNaN(finalL6))
-    {
-      alert("Please make sure you input your grades before asking for a prediction");
+   
+
+    
+    //where magic happens
+    var totalAverage = parseFloat(finalL5.toFixed(2)) + parseFloat(finalL6.toFixed(2));
+
+    var final_year_planning = document.getElementById("final_year_planning");
+    var firstClassRad = document.getElementById("first_class");
+    var upperSecondClassRad = document.getElementById("upper_second_class");
+    var lowerSecondClassRad = document.getElementById("lower_second_class");
+    var thirdClassRad = document.getElementById("third_class");
+    var passRad = document.getElementById("pass");
+    //get a reference to each radio button then with ifs check which one is clicked and calculate and display text accordingly
+
+    if(final_year_planning.checked){
+      if (firstClassRad.checked)
+      {
+        firstPrediction = parseFloat(firstClass) - parseFloat(totalAverage);
+
+        if (firstPrediction < 0)
+        {
+          document.getElementById("predict").innerHTML = "Well Done! First class achieved, you are over by " + Math.abs(firstPrediction.toFixed(2)) + "%";
+        }
+        else
+        {
+          L5extra = parseFloat(firstPrediction) / 2 * 10;
+          L6extra = parseFloat(firstPrediction) / 8 * 10;
+          document.getElementById("predict").innerHTML = "You have not yet achieved a first class, to do so you need an extra " + firstPrediction.toFixed(2) + "%" +
+                                                        " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
+        }
+        
+      }
+      else if(upperSecondClassRad.checked)
+      {
+        upperSecondPrediction = parseFloat(secondUpper) - parseFloat(totalAverage);
+
+        if (upperSecondPrediction < 0)
+        {
+          document.getElementById("predict").innerHTML = "Well Done! Upper second class achieved, you are over by " + Math.abs(upperSecondPrediction.toFixed(2)) + "%";
+        }
+        else
+        {
+          L5extra = parseFloat(upperSecondPrediction) / 2 * 10;
+          L6extra = parseFloat(upperSecondPrediction) / 8 * 10;
+          document.getElementById("predict").innerHTML = "You have not yet achieved an upper second class, to do so you need an extra " + upperSecondPrediction.toFixed(2) + "%" +
+                                                        " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
+        }
+      }
+      else if(lowerSecondClassRad.checked)
+      {
+        lowerSecondPrediction = parseFloat(secondLower) - parseFloat(totalAverage);
+
+        if (lowerSecondPrediction < 0)
+        {
+          document.getElementById("predict").innerHTML = "Well Done! Lower second class achieved, you are over by " + Math.abs(lowerSecondPrediction.toFixed(2)) + "%";
+        }
+        else
+        {
+          L5extra = parseFloat(lowerSecondPrediction) / 2 * 10;
+          L6extra = parseFloat(lowerSecondPrediction) / 8 * 10;
+          document.getElementById("predict").innerHTML = "You have not yet achieved a lower second class, to do so you need an extra " + lowerSecondPrediction.toFixed(2) + "%" +
+                                                        " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
+        }
+      }
+      else if(thirdClassRad.checked)
+      {
+        thirdPrediction = parseFloat(third) - parseFloat(totalAverage);
+
+        if (thirdPrediction < 0)
+        {
+          document.getElementById("predict").innerHTML = "Well Done! Third class achieved, you are over by " + Math.abs(thirdPrediction.toFixed(2)) + "%";
+        }
+        else
+        {
+          L5extra = parseFloat(thirdPrediction) / 2 * 10;
+          L6extra = parseFloat(thirdPrediction) / 8 * 10;
+          document.getElementById("predict").innerHTML = "You have not yet achieved a third class, to do so you need an extra " + thirdPrediction.toFixed(2) + "%" +
+                                                        " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
+        }
+      }
+      else if(passRad.checked)
+      {
+        passPrediction = parseFloat(pass) - parseFloat(totalAverage);
+
+        if (passPrediction < 0)
+        {
+          document.getElementById("predict").innerHTML = "Well Done! Pass achieved, you are over by " + Math.abs(passPrediction.toFixed(2)) + "%";
+        }
+        else
+        {
+          L5extra = parseFloat(passPrediction) / 2 * 10;
+          L6extra = parseFloat(passPrediction) / 8 * 10;
+          document.getElementById("predict").innerHTML = "You have not yet achieved a pass, to do so you need an extra " + passPrediction.toFixed(2) + "%" +
+                                                        " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
+        }
+      }
     }
-    else
-    {
-      //where magic happens
-      var totalAverage = parseFloat(finalL5.toFixed(2)) + parseFloat(finalL6.toFixed(2));
-
-      var final_year_planning = document.getElementById("final_year_planning");
-      var firstClassRad = document.getElementById("first_class");
-      var upperSecondClassRad = document.getElementById("upper_second_class");
-      var lowerSecondClassRad = document.getElementById("lower_second_class");
-      var thirdClassRad = document.getElementById("third_class");
-      var passRad = document.getElementById("pass");
-      //get a reference to each radio button then with ifs check which one is clicked and calculate and display text accordingly
-
-      if(final_year_planning.checked){
-        if (firstClassRad.checked)
-        {
-          firstPrediction = parseFloat(firstClass) - parseFloat(totalAverage);
-  
-          if (firstPrediction < 0)
-          {
-            document.getElementById("predict").innerHTML = "Well Done! First class achieved, you are over by " + Math.abs(firstPrediction.toFixed(2)) + "%";
-          }
-          else
-          {
-            L5extra = parseFloat(firstPrediction) / 2 * 10;
-            L6extra = parseFloat(firstPrediction) / 8 * 10;
-            document.getElementById("predict").innerHTML = "You have not yet achieved a first class, to do so you need an extra " + firstPrediction.toFixed(2) + "%" +
-                                                          " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
-          }
-          
-        }
-        else if(upperSecondClassRad.checked)
-        {
-          upperSecondPrediction = parseFloat(secondUpper) - parseFloat(totalAverage);
-
-          if (upperSecondPrediction < 0)
-          {
-            document.getElementById("predict").innerHTML = "Well Done! Upper second class achieved, you are over by " + Math.abs(upperSecondPrediction.toFixed(2)) + "%";
-          }
-          else
-          {
-            L5extra = parseFloat(upperSecondPrediction) / 2 * 10;
-            L6extra = parseFloat(upperSecondPrediction) / 8 * 10;
-            document.getElementById("predict").innerHTML = "You have not yet achieved an upper second class, to do so you need an extra " + upperSecondPrediction.toFixed(2) + "%" +
-                                                          " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
-          }
-        }
-        else if(lowerSecondClassRad.checked)
-        {
-          lowerSecondPrediction = parseFloat(secondLower) - parseFloat(totalAverage);
-
-          if (lowerSecondPrediction < 0)
-          {
-            document.getElementById("predict").innerHTML = "Well Done! Lower second class achieved, you are over by " + Math.abs(lowerSecondPrediction.toFixed(2)) + "%";
-          }
-          else
-          {
-            L5extra = parseFloat(lowerSecondPrediction) / 2 * 10;
-            L6extra = parseFloat(lowerSecondPrediction) / 8 * 10;
-            document.getElementById("predict").innerHTML = "You have not yet achieved a lower second class, to do so you need an extra " + lowerSecondPrediction.toFixed(2) + "%" +
-                                                          " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
-          }
-        }
-        else if(thirdClassRad.checked)
-        {
-          thirdPrediction = parseFloat(third) - parseFloat(totalAverage);
-
-          if (thirdPrediction < 0)
-          {
-            document.getElementById("predict").innerHTML = "Well Done! Third class achieved, you are over by " + Math.abs(thirdPrediction.toFixed(2)) + "%";
-          }
-          else
-          {
-            L5extra = parseFloat(thirdPrediction) / 2 * 10;
-            L6extra = parseFloat(thirdPrediction) / 8 * 10;
-            document.getElementById("predict").innerHTML = "You have not yet achieved a third class, to do so you need an extra " + thirdPrediction.toFixed(2) + "%" +
-                                                          " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
-          }
-        }
-        else if(passRad.checked)
-        {
-          passPrediction = parseFloat(pass) - parseFloat(totalAverage);
-
-          if (passPrediction < 0)
-          {
-            document.getElementById("predict").innerHTML = "Well Done! Pass achieved, you are over by " + Math.abs(passPrediction.toFixed(2)) + "%";
-          }
-          else
-          {
-            L5extra = parseFloat(passPrediction) / 2 * 10;
-            L6extra = parseFloat(passPrediction) / 8 * 10;
-            document.getElementById("predict").innerHTML = "You have not yet achieved a pass, to do so you need an extra " + passPrediction.toFixed(2) + "%" +
-                                                          " this would mean an extra " + L5extra.toFixed(2) + "% in level 5 or an extra " + L6extra.toFixed(2) + "% in level 6";
-          }
-        }
-      }
-      else{
-        document.getElementById("predict").innerHTML = "";
-      }
+    else{
+      document.getElementById("predict").innerHTML = "";
+    }
 
       
-  }    
+    
 }
     
 
